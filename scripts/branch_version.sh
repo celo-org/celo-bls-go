e7a4a3459b25acaf36b77c551d05b587dc23ab7b
Kobi Gurkan <kobigurk@gmail.com>
2020-05-15 20:07:25 +0300

    uses new name for submodule

--- scripts/branch_version.sh
+++ scripts/branch_version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-pushd bls-zexe
+pushd celo-bls-snark-rs
 export VERSION_FROM_CARGO=v`cargo pkgid -p epoch-snark | cut -d'#' -f2 | cut -d: -f2`
 export VERSION=${1:-${VERSION_FROM_CARGO}}
 git checkout ${VERSION}
