849e8753e3976a1f9702b714df736d7665d0de6e
Victor Graf <victor@clabs.co>
2022-06-03 16:52:23 -0700

    render templates

--- bls/bls_other.go
+++ bls/bls_other.go
@@ -1,10 +1,9 @@
-//go:build s390x
 // +build s390x
 
 package bls
 
 import (
-	blsRoute "github.com/celo-org/celo-bls-go-other/bls"
+    blsRoute "github.com/celo-org/celo-bls-go-other/bls"
 )
 
 const (
@@ -32,81 +31,81 @@ type SignedBlockHeader = blsRoute.SignedBlockHeader
 type EpochEntropy = blsRoute.EpochEntropy
 
 func InitBLSCrypto() {
-	blsRoute.InitBLSCrypto()
+    blsRoute.InitBLSCrypto()
 }
 
 func GeneratePrivateKey() (*PrivateKey, error) {
-	return blsRoute.GeneratePrivateKey()
+    return blsRoute.GeneratePrivateKey()
 }
 
 func DeserializePrivateKey(privateKeyBytes []byte) (*PrivateKey, error) {
-	return blsRoute.DeserializePrivateKey(privateKeyBytes)
+    return blsRoute.DeserializePrivateKey(privateKeyBytes)
 }
 
 func HashDirect(message []byte, usePoP bool) ([]byte, error) {
-	return blsRoute.HashDirect(message, usePoP)
+    return blsRoute.HashDirect(message, usePoP)
 }
 
 func HashDirectWithAttempt(message []byte, usePoP bool) ([]byte, uint, error) {
-	return blsRoute.HashDirectWithAttempt(message, usePoP)
+    return blsRoute.HashDirectWithAttempt(message, usePoP)
 }
 
 func HashComposite(message []byte, extraData []byte) ([]byte, error) {
-	return blsRoute.HashComposite(message, extraData)
+    return blsRoute.HashComposite(message, extraData)
 }
 
 func HashDirectFirstStep(message []byte, hashBytes int32) ([]byte, error) {
-	return blsRoute.HashDirectFirstStep(message, hashBytes)
+    return blsRoute.HashDirectFirstStep(message, hashBytes)
 }
 
 func HashCRH(message []byte, hashBytes int32) ([]byte, error) {
-	return blsRoute.HashCRH(message, hashBytes)
+    return blsRoute.HashCRH(message, hashBytes)
 }
 
 func HashCompositeCIP22(message []byte, extraData []byte) ([]byte, uint8, error) {
-	return blsRoute.HashCompositeCIP22(message, extraData)
+    return blsRoute.HashCompositeCIP22(message, extraData)
 }
 
 func CompressSignature(signature []byte) ([]byte, error) {
-	return blsRoute.CompressSignature(signature)
+    return blsRoute.CompressSignature(signature)
 }
 
 func CompressPublickey(pubkey []byte) ([]byte, error) {
-	return blsRoute.CompressPublickey(pubkey)
+    return blsRoute.CompressPublickey(pubkey)
 }
 
 func DeserializePublicKey(publicKeyBytes []byte) (*PublicKey, error) {
-	return blsRoute.DeserializePublicKey(publicKeyBytes)
+    return blsRoute.DeserializePublicKey(publicKeyBytes)
 }
 
 func DeserializePublicKeyCached(publicKeyBytes []byte) (*PublicKey, error) {
-	return blsRoute.DeserializePublicKeyCached(publicKeyBytes)
+    return blsRoute.DeserializePublicKeyCached(publicKeyBytes)
 }
 
 func BatchVerifyEpochs(signedHeaders []*SignedBlockHeader, shouldUseCompositeHasher, shouldUseCIP22 bool) error {
-	return blsRoute.BatchVerifyEpochs(signedHeaders, shouldUseCompositeHasher, shouldUseCIP22)
+    return blsRoute.BatchVerifyEpochs(signedHeaders, shouldUseCompositeHasher, shouldUseCIP22)
 }
 
 func DeserializeSignature(signatureBytes []byte) (*Signature, error) {
-	return blsRoute.DeserializeSignature(signatureBytes)
+    return blsRoute.DeserializeSignature(signatureBytes)
 }
 
 func AggregatePublicKeys(publicKeys []*PublicKey) (*PublicKey, error) {
-	return blsRoute.AggregatePublicKeys(publicKeys)
+    return blsRoute.AggregatePublicKeys(publicKeys)
 }
 
 func AggregatePublicKeysSubtract(aggregatedPublicKey *PublicKey, publicKeys []*PublicKey) (*PublicKey, error) {
-	return blsRoute.AggregatePublicKeysSubtract(aggregatedPublicKey, publicKeys)
+    return blsRoute.AggregatePublicKeysSubtract(aggregatedPublicKey, publicKeys)
 }
 
 func AggregateSignatures(signatures []*Signature) (*Signature, error) {
-	return blsRoute.AggregateSignatures(signatures)
+    return blsRoute.AggregateSignatures(signatures)
 }
 
 func EncodeEpochToBytesCIP22(epochIndex uint16, round uint8, blockHash, parentHash EpochEntropy, maximumNonSigners, maximumValidators uint32, addedPublicKeys []*PublicKey) ([]byte, []byte, error) {
-	return blsRoute.EncodeEpochToBytesCIP22(epochIndex, round, blockHash, parentHash, maximumNonSigners, maximumValidators, addedPublicKeys)
+    return blsRoute.EncodeEpochToBytesCIP22(epochIndex, round, blockHash, parentHash, maximumNonSigners, maximumValidators, addedPublicKeys)
 }
 
 func EncodeEpochToBytes(epochIndex uint16, maximumNonSigners uint32, addedPublicKeys []*PublicKey) ([]byte, error) {
-	return blsRoute.EncodeEpochToBytes(epochIndex, maximumNonSigners, addedPublicKeys)
+    return blsRoute.EncodeEpochToBytes(epochIndex, maximumNonSigners, addedPublicKeys)
 }
