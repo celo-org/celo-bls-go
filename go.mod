29f12168c0ae2a95efbf8ec06be2d410eeb9f02b
Michael Straka <mstraka100@gmail.com>
2022-06-29 16:58:08 -0500

    remove mips

--- go.mod
+++ go.mod
@@ -3,10 +3,9 @@ module github.com/celo-org/celo-bls-go
 go 1.12
 
 require (
-	github.com/celo-org/celo-bls-go-linux v0.5.1
-	github.com/celo-org/celo-bls-go-macos v0.5.1
 	github.com/celo-org/celo-bls-go-android v0.5.1
 	github.com/celo-org/celo-bls-go-ios v0.5.1
-	github.com/celo-org/celo-bls-go-windows v0.5.1
+	github.com/celo-org/celo-bls-go-macos v0.5.1
 	github.com/celo-org/celo-bls-go-other v0.5.1
+	github.com/celo-org/celo-bls-go-windows v0.5.1
 )
--- scripts/release.sh
+++ scripts/release.sh
@@ -14,7 +14,7 @@ rustup default 1.52.1
 cargo install cargo-lipo -f
 cargo install cargo-strip -f
 
-declare -a platforms=("i686-unknown-linux-gnu" "x86_64-unknown-linux-gnu" "arm-unknown-linux-gnueabi" "arm-unknown-linux-gnueabihf" "aarch64-unknown-linux-gnu" "mips-unknown-linux-gnu" "mipsel-unknown-linux-gnu" "mips64-unknown-linux-gnuabi64" "mips64el-unknown-linux-gnuabi64" "x86_64-apple-darwin" "aarch64-apple-darwin" "i686-pc-windows-gnu" "x86_64-pc-windows-gnu" "aarch64-linux-android" "armv7-linux-androideabi" "i686-linux-android" "x86_64-linux-android" "x86_64-unknown-linux-musl" "s390x-unknown-linux-gnu" "aarch64-apple-ios x86_64-apple-ios" )
+declare -a platforms=("i686-unknown-linux-gnu" "x86_64-unknown-linux-gnu" "arm-unknown-linux-gnueabi" "arm-unknown-linux-gnueabihf" "aarch64-unknown-linux-gnu" "x86_64-apple-darwin" "aarch64-apple-darwin" "i686-pc-windows-gnu" "x86_64-pc-windows-gnu" "aarch64-linux-android" "armv7-linux-androideabi" "i686-linux-android" "x86_64-linux-android" "x86_64-unknown-linux-musl" "s390x-unknown-linux-gnu" "aarch64-apple-ios x86_64-apple-ios" )
 
 for platform in ${platforms[@]}; do
   rustup target add $platform
